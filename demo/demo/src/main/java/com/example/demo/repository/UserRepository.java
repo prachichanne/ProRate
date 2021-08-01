c9144855d92ce42c67b1518ade2039c671b71b03
prachichanne <72873626+prachichanne@users.noreply.github.com>
2021-08-01 13:32:18 +0530

    Delete demo/demo/src/main directory

--- demo/demo/src/main/java/com/example/demo/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.document.Users;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface  UserRepository extends MongoRepository<Users,Integer> {
-
-}
